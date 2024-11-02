c8972e0e048ebd6fa28d3e9da9865e601cbf4cf2
“darek632” <162553961+darek632@users.noreply.github.com>
2024-11-02 10:25:49 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const body = document.querySelector("body");
+const para = document.querySelector("p");
+
+para.textContent = "Welcome testing testing text content via js";
+
+body.appendChild(para);
