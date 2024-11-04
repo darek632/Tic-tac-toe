b211fd1a80328c17c2c982dcbe89814277c670c9
“darek632” <162553961+darek632@users.noreply.github.com>
2024-11-04 16:23:24 +0000

    create draft logic for 2d array grid function

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 const body = document.querySelector("body");
 const para = document.querySelector("p");
 
-para.textContent = "Welcome testing testing text content via js";
+// para.textContent = "Welcome testing testing text content via js";
 
-body.appendChild(para);
+// body.appendChild(para);
+
+
+function Gameboard() { 
+    const grid = []; 
+    rows = 3;
+    columns = 3;
+
+    const createGrid = function() { 
+
+        for (i=0; i < rows; i++) { 
+
+        grid[i] = [];
+        console.log(grid);
+            for (let j=0; j < columns; j++) { 
+                grid[i][j] = 0;
+                console.log(grid);
+            }
+        }
+
+    }
+
+    return {createGrid};
+
+}
+
+const theGrid = Gameboard();
+
+theGrid.createGrid();
